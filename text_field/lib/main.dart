2edf224fba3e5563ef4f8e060745fc5096ef50db
Nikhil Taral <nikhtaral@gmail.com>
2026-06-11 12:23:23 +0530

    Create

---
+++ text_field/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: TextFieldDemo());
+  }
+}
+
+class TextFieldDemo extends StatefulWidget {
+  const TextFieldDemo({super.key});
+
+  @override
+  State<TextFieldDemo> createState() => _TextFieldDemoState();
+}
+
+class _TextFieldDemoState extends State<TextFieldDemo> {
+  final TextEditingController controller = TextEditingController();
+  final FocusNode focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('TextFormField Test')),
+      body: Container(
+        constraints: BoxConstraints(maxWidth: 500),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: TextFormField(
+            controller: controller,
+            focusNode: focusNode,
+            autofocus: false,
+            readOnly: false,
+            textAlign: TextAlign.start,
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.done,
+            enableInteractiveSelection: true,
+            decoration: const InputDecoration(
+              labelText: 'Enter Text',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
